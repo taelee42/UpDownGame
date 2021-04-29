b912c9ca1644602deff98a89d73fc2b14fac09a8
taelee <ritmakid@gmail.com>
2021-04-29 09:42:40 +0900

    first commit

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sender.value = Float(integerValue)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+     
+        
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        if hitValue == randomValue {
+            print("You hit!")
+            reset()
+        } else if tryCount >= 5 {
+            print("You lose...")
+            reset()
+        }else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else if randomValue < hitValue {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        slider.value = 15
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        tryCount = 0
+        slider.value = 15
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+        tryCountLabel.text = "0 / 5"
+    }
 }
 
